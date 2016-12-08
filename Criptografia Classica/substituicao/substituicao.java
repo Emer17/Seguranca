a0f5eabfcb98dfe7e4004d7e480c80f0b917fb9e
Emer17 <emer-martins@hotmail.com>
2016-12-08 09:48:14 -0200

    Criptografia Classica: adicionado substituicao

---
+++ Criptografia Classica/substituicao/substituicao.java
@@ -0,0 +1,32 @@
+package substituicao;
+import java.io.*;
+import java.nio.file.*;
+public class substituicao{
+    public byte[] entrada;
+    public byte[] key;
+    public char[] ascii = new char[256];
+    public substituicao(byte[] v) throws IOException{
+        this.entrada = v;// esse é o vetor que recebe a entrada.
+        this.key = Files.readAllBytes(Paths.get("keys/key1"));
+        for(int a = 0; a < 256; a++)
+            ascii[a] = (char) a;
+    }
+    public void criptografar() throws IOException{
+        FileOutputStream w = new FileOutputStream(new File("substituicao/outputs/out_crip.txt"));
+        for(int u = 0; u < this.entrada.length; u++)
+            this.entrada[u] = this.key[(int)this.entrada[u]];
+        w.write(this.entrada);
+        w.close();
+    }
+    public void descriptografar() throws IOException{
+        FileOutputStream w = new FileOutputStream(new File("substituicao/outputs/out_descrip.txt"));
+        //System.out.println("tamanho: " +  hugo[3]+ " " + this.entrada[3]);
+        for(int u = 0; u < entrada.length; u++){
+            //System.out.println("sdads " + Math.abs((int)this.entrada[u]));
+            this.entrada[u] = (byte)this.ascii[Math.abs((int)this.entrada[u])];
+        }
+            //this.entrada[u] = this.key[(int)this.entrada[u]];
+        w.write(this.entrada);
+        w.close();
+    }
+}
